07e42f9f52b5714a730c75d71febe2c3d627016f
Damian Sypniewski <damian@sypniew.ski>
2024-08-15 17:53:57 +0900

    Fix armeria context not updated issue

--- tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/curiostack/ToolDependencies.java
+++ tools/gradle-plugins/gradle-curiostack-plugin/src/main/java/org/curioswitch/gradle/plugins/curiostack/ToolDependencies.java
@@ -33,7 +33,7 @@ public class ToolDependencies {
 
   private static final Map<String, String> DEFAULT_VERSIONS =
       ImmutableMap.<String, String>builder()
-          .put("bom", "0.11.11")
+          .put("bom", "0.11.12")
           .put("google-java-format", "1.15.0")
           .build();
 
